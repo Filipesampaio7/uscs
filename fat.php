3e2d7cb09fc08fe799a736054b0554468c35173b
Filipesampaio7 <113558872+Filipesampaio7@users.noreply.github.com>
2022-09-16 21:14:56 -0300

    Add files via upload

---
+++ fat.php
@@ -0,0 +1,16 @@
+<html>
+	<body>
+			<h1> fatorial </h1>
+			<?php
+			$fatorial =1;
+			$n = 6;
+			while ($n >1)
+			{
+			$fatorial = $fatorial * $n;
+			$--;
+			}
+			echo "o fatorial de 6 e $fatorial"
+		?>
+	
+	</body>
+</html>
